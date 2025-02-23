3b4c7c340f7c27b54982e1c37df6c1b1c2b6984f
Việt Huỳnh <vietthedev@gmail.com>
2025-02-24 03:43:57 +0700

    build: add path mapping

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 
-import App from "./App";
+import App from "@/App";
 
 describe(App.name, () => {
 	it("can render", () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 	test: {
 		environment: "jsdom",
 		globals: true,
